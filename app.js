763c507e5eff52c8f01c05482512551b7438008d
Sivasankar <sivasankar.d@sap.com>
2016-04-12 13:33:30 +0530

    Add to cart functionality is done
    TODO:
    	1. Handle remove from cart
    	2. Updating current status
    	3. Convert it into Chrome Extension
    	4. Proper Readme for devs
    	5. UI improvements

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require("express");
+var cors = require("cors");
 
 var app = express();
 
-var PORT = 6000;
+var PORT = 9393;
 
 app.use(function(req, res, next){
 	console.log(`${req.method} -> ${req.url}`);
 	next();
 });
-
+app.use(cors());
 app.use(express.static("./"));
 
-app.listen(6000);
+app.listen(PORT);
 
 console.log(`Serving @ ${PORT}`);
+
+module.exports = app;
--- js/MiFlashSale.js
+++ js/MiFlashSale.js
@@ -6,14 +6,13 @@
  * instagram: @hisivasankar
  */
 
-var FlashSaleHandler = function() {
+var FlashSaleHandler = function(itemToBeAdded) {
+    this._item = itemToBeAdded;
     this._date = new Date();
     this._timer = null;
+    this._counter = 0;
 };
 
-FlashSaleHandler.prototype.startScript = function(interval) {
-    this._timer = setInterval(this.startShopping.bind(this), interval);
-};
 FlashSaleHandler.prototype.stopScript = function(mins, seconds) {
     var timeout;
     if (mins) {
@@ -36,31 +35,56 @@ FlashSaleHandler.prototype.setStartTime = function(hour, mins) {
         mins: mins
     };
 };
-FlashSaleHandler.prototype.startShopping = function() {
+FlashSaleHandler.prototype.startShoppingDontUseThis = function(interval) {
 
-    var hour = this._date.getHours();
-    var mins = this._date.getMinutes();
+    this._timer = setInterval(function() {
 
-    if (hour >= this._saleStarts.hours) {
-        var selectorCollection = {
-            "flashSale": ".flash-sale-list.sale-list li:nth-child(3) > a",
-            "brandNewAccessories": ".news-list.sale-list li:nth-child(3) > a"
-        };
-        var addToChartButton = $(selectorCollection.brandNewAccessories)[0];
+        var hour = this._date.getHours();
+        var mins = this._date.getMinutes();
 
-        var status = "Out of stock";
-        if (addToChartButton && addToChartButton.text != status) {
-            console.log(addToChartButton.text);
-            addToChartButton.click();
-            Console.log("Hey, Check your chart! I guess it's added");
-            this.stopScript(null, 10);
+        if (hour >= this._saleStarts.hours) {
+            var selectorCollection = {
+                "flashSale": ".flash-sale-list.sale-list li:nth-child(3) > a",
+                "brandNewAccessories": ".news-list.sale-list li:nth-child(3) > a"
+            };
+            var addToChartButton = $(selectorCollection.brandNewAccessories)[0];
+
+            var status = "Out of stock";
+            if (addToChartButton && addToChartButton.text != status) {
+                console.debug(addToChartButton.text);
+                addToChartButton.click();
+                Console.debug("Hey, Check your chart! I guess it's added");
+                this.stopScript(null, 10);
+            } else {
+                var itemCaption = addToChartButton.text;
+                console.debug("Not able to added to chart, Item says :", itemCaption);
+            }
+            console.debug("Sale Started!");
         } else {
-            var itemCaption = addToChartButton.text;
-            console.log("Not able to added to chart, Item says :", itemCaption);
+            console.debug("Not Stated!");
         }
-        console.log("Sale Started!");
+
+    }.bind(this), interval);
+};
+
+FlashSaleHandler.prototype.checkAndAdd = function(interval) {
+    if (this._item) {
+        this._timer = setInterval(function() {
+            var status = "Out of Stock";
+            var currentStatus = this._item.text;
+            if (status.toLowerCase() != currentStatus.toLowerCase()) {
+                this._item.click();
+                console.debug("Item Added : ", this._item._dataTitle);
+                this.stopScript(null, 2);
+            } else {
+                this._counter++;
+                var reportStatus = "Checking... " + this._counter;
+                this._item._dataStatusIndicator.innerHTML = reportStatus;
+                console.debug(status);
+            }
+        }.bind(this), interval);
     } else {
-        console.log("Not Stated!");
+        console.debug("Item reference not available");
     }
 };
 
@@ -72,51 +96,69 @@ FlashSaleManager.prototype.findAllSale = function() {
 
     var saleSelector = "a.btn-link";
 
-    var getBadge = function() {
-    	var addButton = document.createElement("a");
-    	addButton.appendChild(document.createTextNode("Add Me"));
-    	addButton.setAttribute("class", "addToChart");
+    var getBadge = function(item) {
 
-    	addButton.style.background = "rgb(255, 255, 255)";
-    	addButton.style.color = "black";
-    	addButton.style.fontSize = "18px";
-    	addButton.style.padding = "3px";
-    	addButton.style.margin = "3px";
+        var shoppingItemTitle = $(item).parent().children("p").text();
+        item._dataTitle = shoppingItemTitle;
 
-    	var removeButton = document.createElement("a");
-    	removeButton.appendChild(document.createTextNode("Remove Me"));
-    	removeButton.setAttribute("class", "removeFromCart");
+        var addButton = document.createElement("a");
+        addButton.appendChild(document.createTextNode("Add Me"));
+        addButton.setAttribute("class", "addToCart");
 
-    	removeButton.style.background = "rgb(255, 255, 255)";
-    	removeButton.style.color = "black";
-    	removeButton.style.fontSize = "18px";
-    	removeButton.style.margin = "3px";
-    	removeButton.style.padding = "3px";
+        addButton._flashSaleHandler = new FlashSaleHandler(item);
 
-    	var badgeContainer = document.createElement("div");
-    	badgeContainer.setAttribute("class", "badgeContainer");
+        addButton.onclick = function() {
+            this._flashSaleHandler.checkAndAdd(500);
+        };
 
-    	badgeContainer.appendChild(addButton);
-    	badgeContainer.appendChild(removeButton);
+        addButton.style.background = "#757575";
+        addButton.style.color = "white";
+        addButton.style.fontSize = "18px";
+        addButton.style.padding = "3px";
+        addButton.style.margin = "3px";
 
-    	badgeContainer.style.margin = "5px";
+        var removeButton = document.createElement("a");
+        removeButton.appendChild(document.createTextNode("Remove Me"));
+        removeButton.setAttribute("class", "removeFromCart");
 
-    	return badgeContainer;
+        removeButton.style.background = "#757575";
+        removeButton.style.color = "white";
+        removeButton.style.fontSize = "18px";
+        removeButton.style.margin = "3px";
+        removeButton.style.padding = "3px";
+
+        var statusIndicator = document.createElement("p");
+        //statusIndicator.appendChild(document.createTextNode("Checking"));
+
+        item._dataStatusIndicator = statusIndicator;
+
+        var badgeContainer = document.createElement("div");
+        badgeContainer.setAttribute("class", "badgeContainer");
+        badgeContainer.style.marginTop = "10px";
+
+        badgeContainer.appendChild(addButton);
+        badgeContainer.appendChild(removeButton);
+        badgeContainer.appendChild(statusIndicator);
+
+
+        return badgeContainer;
     };
 
     $(saleSelector).each(function(item) {
-    	var badge = getBadge();
+        var badge = getBadge(this);
         $(badge).insertAfter(this);
-        console.log("Adding something");
+        console.debug("Adding something");
     });
 
 };
 
 
 (function() {
-    var flashSale = new FlashSaleHandler();
+    /*var flashSale = new FlashSaleHandler();
     flashSale.setStartTime(12, 0);
     flashSale.startScript(500);
     flashSale.stopScript(1);
-    window.flashSale = flashSale;
+    window.flashSale = flashSale;*/
+    var flashSaleManager = new FlashSaleManager();
+    flashSaleManager.findAllSale();
 })();
